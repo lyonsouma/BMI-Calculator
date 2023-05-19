9bd665bfea65ffff4a3f6db6aff4fd32d690a03c
Lyons Ouma <oumalyons26@gmail.com>
2023-05-19 16:08:38 +0300

    input fields for weight and height

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Content() {
+  const[age, setAge]=useState('');
+  const[weight, setWeight]=useState('');
+  const[height, setHeight]=useState('');
   return (
-    <div></div>
+    <div>
+      <input type='number' placeholder='Enter Your Age ' value={age} onChange={(e)=>setAge(e.target.value)}/>
+      <input type='number' placeholder='Enter Your Weight ' value={weight} onChange={(e)=>setWeight(e.target.value)}/>
+      <input type='number' placeholder='Enter Your height ' value={height} onChange={(e)=>setHeight(e.target.value)}/>
+    </div>
   )
 }
 
