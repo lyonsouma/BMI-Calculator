383cc6b99634876a459bde807e7cf24b4b114a74
Lyons Ouma <oumalyons26@gmail.com>
2023-05-19 15:50:19 +0300

    added content component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Content from './components/Content';
 import Navbars from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
      <Navbars />
+     <div>
+      <Content />
+     </div>
     </div>
   );
 }
