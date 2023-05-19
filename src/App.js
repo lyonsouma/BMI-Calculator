7b8ebdb440ff508d30bf25f9b12a4a0192be6fd5
Lyons Ouma <oumalyons26@gmail.com>
2023-05-19 13:03:37 +0300

    Navbar for BMI app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbars from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+     <Navbars />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar =()=> {
+const Navbars =()=> {
   return (
-    <div>Navbar</div>
+    <div>
+        {/* Navbar */}
+        <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Body Mass Index App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+          </Nav>
+          <Form className="d-flex">
+            <Button variant="outline-success">Calculate</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbars
